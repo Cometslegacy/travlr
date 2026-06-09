37e602ab6857d99e21486302566144931b15c269
Cometslegacy <blackcomet9@gmail.com>
2026-06-09 05:46:58 -0400

    module 6 updates
    up to page 160

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { trips } from '../data/trips'
+import { TripCard } from '../trip-card/trip-card';
 
 @Component({
   selector: 'app-trip-listing',
-  imports: [ CommonModule],
+  standalone: true,
+  imports: [ CommonModule, TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
 })
 
 export class TripListing implements OnInit {
   trips: Array<any> = trips
+
+  constructor() {}
 
   ngOnInit(): void {
 
